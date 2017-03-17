72c630f64321870b1ae7ed1febe023510755706f
Prashasthi Prabhakar <prashasthip@gmail.com>
2017-03-17 13:38:33 -0700

    simple topology

--- baas_project.py
+++ baas_project.py
@@ -11,6 +11,8 @@ topology enables one to pass in '--topo=mytopo' from the command line.
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.cli import CLI
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
 
 class MyTopo( Topo ):
     "Simple topology example."
@@ -20,10 +22,11 @@ class MyTopo( Topo ):
 
         # Initialize topology
         Topo.__init__( self )
-	print "hello"
-        home_link_config = {'bw': 25, 'delay' :'5ms',  
-		'max_queue_size' : 1000, 'use_htb' : True}
-        isp_link_config = {'bw': 200, 'delay' :'5ms', 'max_queue_size' : 1000, 'use_htb' : True}
+
+        home_link_config = {'bw': 25, 'delay' :'5ms', 'loss' : 2, 
+                'max_queue_size' : 1000, 'use_htb' : True}
+        isp_link_config = {'bw': 200, 'delay' :'5ms', 'loss' : 2, 
+                'max_queue_size' : 1000, 'use_htb' : True}
         core_link_config = {'bw': 100, 'delay' :'5ms', 'loss' : 2, 
                 'max_queue_size' : 1000, 'use_htb' : True}
         home_core_link_config = {'bw': 50, 'delay' :'5ms', 'loss' : 2, 
@@ -34,16 +37,15 @@ class MyTopo( Topo ):
         core1 = self.addSwitch('core1')
         for i in range(num_isps):
             isp = self.addHost('H%s' % (i + 1))
-	    print "inside"
             self.addLink( core1, isp, **isp_link_config )
-'''
+
         
         #Create home switches for zone A
         aggrSw1 = self.addSwitch('aggrSw1')
         for i in range(num_homes_per_zone):
             home_switch = self.addSwitch('A%s' % (i + 1))
             self.addLink( aggrSw1, home_switch, **home_link_config)
-	print "zz"
+
         #Create home switches for zone B
         aggrSw2 = self.addSwitch('aggrSw2')
         for i in range(num_homes_per_zone):
@@ -60,7 +62,7 @@ class MyTopo( Topo ):
         for i in range(num_homes_per_zone):
             home_switch = self.addSwitch('C%s' % (i + 1))
             self.addLink( aggrSw3, home_switch, **home_link_config)
-	print "ll"
+
         #Create home switches for zone D
         aggrSw4 = self.addSwitch('aggrSw4')
         for i in range(num_homes_per_zone):
@@ -71,18 +73,15 @@ class MyTopo( Topo ):
         core3 = self.addSwitch('core3')
         self.addLink( core3, aggrSw3, **home_core_link_config )
         self.addLink( core3, aggrSw4, **home_core_link_config )
-	
-	print "before core"
+
         #connect the cores
         self.addLink( core1, core2, **core_link_config )
         self.addLink( core2, core3, **core_link_config )
         self.addLink( core3, core1, **core_link_config )            
-	print "core"	
-'''
 
 mytopo = MyTopo(2, 2)
 #topos = { 'mytopo': ( lambda: MyTopo(2, 2) ) }
-net = Mininet( topo=mytopo)
+net = Mininet( topo=mytopo, host=CPULimitedHost, link=TCLink)
 net.start()
 CLI( net )
 net.stop()
