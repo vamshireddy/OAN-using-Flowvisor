13d6634ff2611c951e0887bd208b695bc8abcb6d
vamshi <k.vamshi2008@gmail.com>
2017-03-18 21:05:13 -0700

    Fix the issue, where in the switches are not getting recognized by the controller through flowvisor.
    The problem seems to be from switch dpid representation. Having hexadecimal chars (a, b, c, d, e, f) in the dpid
    is causing the problem.
    
    Fixed by changing swtich dpids to avoid using hex alphabets.

--- baas_project.py
+++ baas_project.py
@@ -37,61 +37,61 @@ class MyTopo( Topo ):
 				'max_queue_size' : 1000, 'use_htb' : True}
         switch_cnt = 1
         #create isps and connect to core switch
-        core1 = self.addSwitch('core-sw1', dpid="%016x" % (switch_cnt))
+        core1 = self.addSwitch('core-sw1', dpid="%d" % (switch_cnt))
         
         for i in range(num_isps):
             isp = self.addHost('coreH%s' % (i + 1))
             self.addLink( core1, isp, **isp_link_config )
         
         #Create home switches for zone A
-        aggrSw1 = self.addSwitch('aggr-sw1', dpid="%016x" % (switch_cnt))
+        aggrSw1 = self.addSwitch('aggr-sw1', dpid="%d" % (switch_cnt))
         for i in range(num_homes_per_zone):
             switch_cnt += 1
-            home_switch = self.addSwitch('hm-swA-%s' % (i + 1), dpid="%016x" % (switch_cnt))
+            home_switch = self.addSwitch('hm-swA-%s' % (i + 1), dpid="%d" % (switch_cnt))
             self.addLink( aggrSw1, home_switch, **home_link_config)
             home_host = self.addHost('hostA%s' % (i + 1))
             self.addLink( home_switch, home_host, **home_inside_link_config )
    
         #Create home switches for zone B
         switch_cnt += 1
-        aggrSw2 = self.addSwitch('aggr-sw2', dpid="%016x" % (switch_cnt))
+        aggrSw2 = self.addSwitch('aggr-sw2', dpid="%d" % (switch_cnt))
         for i in range(num_homes_per_zone):
             switch_cnt += 1
-            home_switch = self.addSwitch('hm-swB-%s' % (i + 1), dpid="%016x" % (switch_cnt))
+            home_switch = self.addSwitch('hm-swB-%s' % (i + 1), dpid="%d" % (switch_cnt))
             self.addLink( aggrSw2, home_switch, **home_link_config )
             home_host = self.addHost('hostB%s' % (i + 1))
             self.addLink( home_switch, home_host, **home_inside_link_config )
         
         #Connect zone A and Zone B to core switch
         switch_cnt += 1
-        core2 = self.addSwitch('core-sw2', dpid="%016x" % (switch_cnt))
+        core2 = self.addSwitch('core-sw2', dpid="%d" % (switch_cnt))
         
         self.addLink( core2, aggrSw1, **home_core_link_config )
         self.addLink( core2, aggrSw2, **home_core_link_config )
 
         #Create home switches for zone C
         switch_cnt += 1
-        aggrSw3 = self.addSwitch('aggr-sw3', dpid="%016x" % (switch_cnt))
+        aggrSw3 = self.addSwitch('aggr-sw3', dpid="%d" % (switch_cnt))
         for i in range(num_homes_per_zone):
             switch_cnt += 1
-            home_switch = self.addSwitch('hm-swC-%s' % (i + 1), dpid="%016x" % (switch_cnt))
+            home_switch = self.addSwitch('hm-swC-%s' % (i + 1), dpid="%d" % (switch_cnt))
             self.addLink( aggrSw3, home_switch, **home_link_config)
             home_host = self.addHost('hostC%s' % (i + 1))
             self.addLink( home_switch, home_host, **home_inside_link_config )
 
         #Create home switches for zone D
         switch_cnt += 1
-        aggrSw4 = self.addSwitch('aggr-sw4', dpid="%016x" % (switch_cnt))
+        aggrSw4 = self.addSwitch('aggr-sw4', dpid="%d" % (switch_cnt))
         for i in range(num_homes_per_zone):
             switch_cnt += 1
-            home_switch = self.addSwitch('hm-swD-%s' % (i + 1), dpid="%016x" % (switch_cnt))
+            home_switch = self.addSwitch('hm-swD-%s' % (i + 1), dpid="%d" % (switch_cnt))
             self.addLink( aggrSw4, home_switch, **home_link_config)
             home_host = self.addHost('hostD%s' % (i + 1))
             self.addLink( home_switch, home_host, **home_inside_link_config )
 
         #Connect zone C and Zone D to core switch
         switch_cnt += 1
-        core3 = self.addSwitch('core-sw3', dpid="%016x" % (switch_cnt))
+        core3 = self.addSwitch('core-sw3', dpid="%d" % (switch_cnt))
         self.addLink( core3, aggrSw3, **home_core_link_config )
         self.addLink( core3, aggrSw4, **home_core_link_config )
         
